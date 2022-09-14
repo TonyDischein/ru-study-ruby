c2f4af18335c55ac482d1cbefa5cf7e88fab0336
Anton Romashkin <tony201994@mail.ru>
2022-09-14 20:53:27 +0400

    Implement my_each, my_map, my_compact and my_reduce methods

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,50 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        index = 0
+        while index < length
+          yield self[index]
+          index += 1
+        end
+        self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        results = self.class.new
+
+        my_each do |item|
+          results << yield(item)
+        end
+
+        results
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        results = self.class.new
+        my_map do |item|
+          results << item unless item.nil?
+        end
+
+        results
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(*args)
+        n = 0
+        if args.count < 1
+          init_value = self[0]
+          n = 1
+        else
+          init_value, = args
+        end
+        for i in n...size
+          init_value = yield init_value, self[i]
+        end
+        init_value
+      end
     end
   end
 end

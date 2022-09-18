c923f574e98fcdc390065bd13d584685eef80c9f
Anton Romashkin <tony201994@mail.ru>
2022-09-18 23:26:16 +0400

    Fix implementation of search method

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -9,43 +9,15 @@ module Exercise
       end
 
       def search(array, query, left = 0, right = nil)
-        right = get_right(array, right)
-        return -1 if end?(left, right)
+        right = array.size - 1 if right.nil?
+        return -1 if left > right
 
-        mid = get_mid(left, right)
-        unless query?(array, mid, query)
-          left, right = update_range(array, query, mid, left, right)
+        mid = (left + ((right - left) / 2)).to_i
+        unless array[mid] == query
+          left, right = array[mid] < query ? [mid + 1, right] : [left, mid - 1]
           return search(array, query, left, right)
         end
         mid
-      end
-
-      private
-
-      def update_range(array, query, mid, left, right)
-        if array[mid] < query
-          left = mid + 1
-        else
-          right = mid - 1
-        end
-        [left, right]
-      end
-
-      def get_right(array, right)
-        right = array.size - 1 if right.nil?
-        right
-      end
-
-      def query?(array, mid, query)
-        array[mid] == query
-      end
-
-      def end?(left, right)
-        left > right
-      end
-
-      def get_mid(left, right)
-        (left + ((right - left) / 2)).to_i
       end
     end
   end

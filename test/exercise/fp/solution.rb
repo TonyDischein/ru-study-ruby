5f172337a4cea0ad505206092e14ee31d3edf82e
Anton Romashkin <tony201994@mail.ru>
2022-09-19 00:16:03 +0400

    Fix rating and chars_count methods

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,20 +5,20 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        films = array.reject { |film| country_nill?(film) }.select { |film| more_two_country?(film) && rating_kinopoisk_positiv?(film) }
+        films = array.select { |film|  more_two_country?(film) && rating_kinopoisk_positiv?(film) }
         films_rating = films.map { |film| only_rating(film) }
         films_rating.reduce { |sum, item| sum + item } / films.count
       end
 
       def chars_count(films, threshold)
-        films = films.select { |film| check_by_rating_and_char?(film, threshold, 'и') }.map { |film| get_char_count(film, 'и') }
+        films = films.select { |film| check_by_rating?(film, threshold) }.map { |film| get_char_count(film, 'и') }
         films.reduce { |sum, item| sum + item }
       end
 
       private
 
-      def check_by_rating_and_char?(film, threshold, char)
-        film['rating_kinopoisk'].to_f >= threshold && film['name'].count(char).positive?
+      def check_by_rating?(film, threshold)
+        film['rating_kinopoisk'].to_f >= threshold
       end
 
       def get_char_count(film, char)
@@ -26,7 +26,7 @@ module Exercise
       end
 
       def more_two_country?(film)
-        film['country'].split(',').size >= 2
+        film['country'].split(',').size >= 2 unless film['country'].nil?
       end
 
       def rating_kinopoisk_positiv?(film)
@@ -35,10 +35,6 @@ module Exercise
 
       def only_rating(film)
         film['rating_kinopoisk'].to_f
-      end
-
-      def country_nill?(film)
-        film['country'].nil?
       end
     end
   end

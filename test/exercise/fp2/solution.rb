3dd42a751288901a3ec73739bc0efa4cb4e24946
Anton Romashkin <tony201994@mail.ru>
2022-09-20 11:41:41 +0400

    Fix my_each, my_map, my_compact and my_reduce methods

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,49 +5,38 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each
-        index = 0
-        while index < length
-          yield self[index]
-          index += 1
+      def my_each(item = 0, &func)
+        unless self[item].nil?
+          func.call self[item]
+          my_each(item + 1, &func)
         end
         self
       end
 
       # Написать свою функцию my_map
       def my_map
-        results = self.class.new
-
-        my_each do |item|
-          results << yield(item)
-        end
-
-        results
+        my_reduce(MyArray.new) { |acc, current| acc << yield(current) }
       end
 
       # Написать свою функцию my_compact
       def my_compact
-        results = self.class.new
-        my_map do |item|
-          results << item unless item.nil?
-        end
-
-        results
+        reduce(self.class.new) { |acc, current| current.nil? ? acc : acc << current }
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce(*args)
-        n = 0
-        if args.count < 1
-          init_value = self[0]
-          n = 1
+      def my_reduce(*args, &func)
+        if args.count.zero?
+          acc = self[0]
+          col = slice(1..length)
         else
-          init_value, = args
+          acc, col = args
+          col = self if col.nil?
         end
-        for i in n...size
-          init_value = yield init_value, self[i]
-        end
-        init_value
+
+        head, *tail = col
+        return acc if head.nil?
+
+        my_reduce(func.call(acc, head), tail, &func)
       end
     end
   end

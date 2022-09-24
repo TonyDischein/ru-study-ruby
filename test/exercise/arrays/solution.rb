c12425a6af59d4ee3334531f43dc2d58f44f1300
Anton Romashkin <tony201994@mail.ru>
2022-09-24 12:03:02 +0400

    Fix implementation of search method

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -12,8 +12,10 @@ module Exercise
         return -1 if array.empty? ||  query < array[0] || query > array[right]
 
         mid = (left + ((right - left) / 2)).to_i
-        left, right = array[mid] < query ? [mid + 1, right] : [left, mid - 1] if array[mid] != query
-        array[mid] == query ? (return mid) : (return search(array, query, left, right))
+        return mid if array[mid] == query
+
+        left, right = array[mid] < query ? [mid + 1, right] : [left, mid - 1]
+        search(array, query, left, right)
       end
     end
   end
